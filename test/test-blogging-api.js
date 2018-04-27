28fde4342c759004b83ef4dceb3ac49d13733863
Caren Keyes <cejkeyes@gmail.com>
2018-04-26 20:40:32 -0700

    add generateBlogData

--- test/test-blogging-api.js
+++ test/test-blogging-api.js
@@ -13,3 +13,21 @@ const {TEST_DATABASE_URL} = require('../config')
 
 chai.use(chaiHttp);
 
+function seedBlogData(){
+    console.info('seeding restaurant data')
+    const seedData = [];
+
+    for (let i=1; i<=10; i++){
+        seedData.push(generateSeedData());
+    }
+    return Restaurant.insertMany(seedData);
+}
+
+function generateBlogData(){
+    return {
+        title: faker.lorem.words(),
+        content: faker.lorem.paragraph(),
+        author: {firstName: faker.name.firstName(), lastName: faker.name.lastName()},
+        created: faker.date.past()
+    };
+}
